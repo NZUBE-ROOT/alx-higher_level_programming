63b6f966d3b6f6889cacc4dae88a1a2ddd1fb805
MomaNzubechukwu <momanzubeolaedo@gmail.com>
2023-01-09 12:46:37 -0800

    help mee oo

--- 0x1A-hash_tables/0-lookup.py
+++ 0x1A-hash_tables/0-lookup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Defines an object attribute lookup function."""
+"""
+Contains the lookup function
+"""
 
 
 def lookup(obj):
-    """Return a list of an object's available attributes."""
-    return (dir(obj))
+    """returns a list of available attributes and methods of an object"""
+    return dir(obj)
