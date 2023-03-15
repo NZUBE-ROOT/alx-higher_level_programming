441f8e184a0eb250183c1f9fd75c159b42d1a377
MomaNzubechukwu <momanzubeolaedo@gmail.com>
2023-03-15 12:27:23 -0700

    tested

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));#!/usr/bin/node
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
