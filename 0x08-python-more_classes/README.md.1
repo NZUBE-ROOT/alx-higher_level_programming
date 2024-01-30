8af5edd2ce550a16cd23a0ddb13dca9697039024
NZUBE-ROOT <momafransisco@gmail.com>
2024-01-29 16:01:45 -0800

    kall

---
+++ 0x08-python-more_classes/README.md.1
@@ -0,0 +1,1 @@
+Task 0x08. Python - More Classes and Objects
---
+++ 0x08-python-more_classes/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir __pycache__
+cd __pycache__
+wget https://raw.githubusercontent.com/Mica-Code/alx-higher_level_programming/main/0x08-python-more_classes/__pycache__/README.md
+chmod +x *
